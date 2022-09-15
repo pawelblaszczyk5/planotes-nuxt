ee44a68a8481b64a3523c8554319a3b9dbb5a50d
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-09-15 20:32:11 +0200

    feat: initial mail service setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,15 +8,6 @@ export default defineNuxtConfig({
 	experimental: {
 		reactivityTransform: true,
 	},
-	runtimeConfig: {
-		DATABASE_URL:
-			'postgresql://postgres:postgres@localhost:5432/planotes' ?? process.env['DATABASE_URL'],
-		DKIM_PRIVATE_KEY: 'dkim_key' ?? process.env['DATABASE_URL'],
-		DKIM_SELECTOR: 'default' ?? process.env['DKIM_SELECTOR'],
-		SMTP_HOST: 'localhost' ?? process.env['SMTP_HOST'],
-		SMTP_USER: 'magic@planotes.xyz' ?? process.env['SMTP_USER'],
-		SMTP_PASSWORD: 'pwd' ?? process.env['SMTP_PASSWORD'],
-	},
 	vite: { devBundler: 'legacy' },
 	unocss: {
 		preflight: true,
---
+++ server/utils/mail.ts
@@ -0,0 +1,55 @@
+import type { Transporter } from 'nodemailer';
+import type SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+import { createTransport } from 'nodemailer';
+import { env } from '~~/server/utils/env';
+
+const transporterConfig: SMTPTransport.Options = {
+	host: env.SMTP_HOST,
+	port: 587,
+	auth: {
+		user: env.SMTP_USER,
+		pass: env.SMTP_PASSWORD,
+	},
+	dkim: {
+		privateKey: env.DKIM_PRIVATE_KEY,
+		keySelector: env.DKIM_SELECTOR,
+		domainName: 'planotes.xyz',
+	},
+};
+
+const transporter: Transporter = createTransport(transporterConfig);
+
+interface SendEmailOptions {
+	html: string;
+	receiver: string;
+	plainTextVersion?: string;
+	senderName: string;
+	subject: string;
+}
+
+interface SendEmailResult {
+	accepted: Array<string>;
+}
+
+export const sendEmail = async ({
+	html,
+	receiver,
+	plainTextVersion,
+	senderName,
+	subject,
+}: SendEmailOptions) => {
+	try {
+		const { accepted }: SendEmailResult = await transporter.sendMail({
+			from: `"${senderName}" <${env.SMTP_USER}>`,
+			to: receiver,
+			subject,
+			text: plainTextVersion,
+			html,
+		});
+
+		if (!accepted.includes(receiver)) throw new Error('Unable to send email');
+	} catch {
+		throw new Error('Unable to send email');
+	}
+};
